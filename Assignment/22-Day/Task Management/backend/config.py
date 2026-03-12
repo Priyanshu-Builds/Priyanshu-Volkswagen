75d38401504ea39b1f71a2cba2c9327b297d5fd3
Priyanshu <priyanshusharma18122002@gmail.com>
2026-03-12 12:08:59 +0530

    Day 22 Assignment Updated

---
+++ Assignment/22-Day/Task Management/backend/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Priyanshu02@localhost/task_management'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'task-management-secret-key'
